a81a751d7180b4cb568f087c82286112eaac500d
Sherlock Moriarty <zhangwenqiang52100@gmail.com>
2016-12-06 20:57:55 +0800

    init version

---
+++ src/main/java/com/zwq/domain/User.java
@@ -0,0 +1,18 @@
+package com.zwq.domain;
+
+/**
+ * Created by Archer on 2016/12/5.
+ */
+public class User {
+    private int id;
+    private String userName;          //登陆名
+    private String nickName;           //昵称
+    private String realName;           //真实姓名
+    private String password;          //密码
+    private String salt;                //盐
+    private String email;              //电子邮件
+    private String email_status;       //邮件状态(是否认证等)
+    private String mobile;                //电话
+
+
+}
